90e115b63b0cc4ed395720212d70f1ef1f5f98a3
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2019-04-24 16:04:51 +0200

    Fix checkstyle of BoardTest

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * A very simple test class to have
+ * for the Board Class.
+ *
+ * @author Shaan Hossain
+ */
 public class BoardTest {
     private Board board;
 
+    /**
+     * Tests if board is correctly instantiated with a (large enough) square.
+     */
     @Test
     public void gridTest() {
         Square[][] sg = new Square[1][1];
