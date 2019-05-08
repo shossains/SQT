ab7833f43e7e7e870d3aeb7cd33d6ea3045dbff1
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2019-05-08 15:58:45 +0200

    Created tests for Clyde

--- src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.jpacman.npc.ghost;
 
+import com.google.common.collect.Lists;
 import nl.tudelft.jpacman.Launcher;
+import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.game.Game;
@@ -8,6 +10,7 @@ import nl.tudelft.jpacman.level.Level;
 import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.level.Player;
 import nl.tudelft.jpacman.level.PlayerFactory;
+import nl.tudelft.jpacman.npc.Ghost;
 import nl.tudelft.jpacman.points.DefaultPointCalculator;
 import nl.tudelft.jpacman.points.PointCalculator;
 import nl.tudelft.jpacman.sprite.PacManSprites;
@@ -16,111 +19,114 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 /**
  * Class to test Clyde behavior.
  */
 public class ClydeTest {
 
-    private Launcher launcher;
     private Boolean temp = true;
     private GhostMapParser ghostMapParser;
-    private Game game;
+    private Player player;
 
+    /**
+     * Set up the GhostMap parser.
+     */
     @BeforeEach
-    void setUp(){
-        PacManSprites pacManSprites = new PacManSprites();
-        BoardFactory boardFactory = new BoardFactory(pacManSprites);
-        GhostFactory ghostFactory = new GhostFactory(pacManSprites);
-        PointCalculator pointCalculator = new DefaultPointCalculator();
-
-        LevelFactory levelFactory = new LevelFactory(pacManSprites, ghostFactory, pointCalculator);
-
-        ghostMapParser = new GhostMapParser(levelFactory, boardFactory, ghostFactory);
-
-        char[][] map = {{'#','#','#','#','#','#','#','#'}, // deprecated layout - GOTO resources/text.txt
-                        {'#',' ',' ',' ',' ',' ',' ','#'},
-                        {'#','P','.','.','.','.','G','#'},
-                        {'#',' ',' ',' ',' ',' ',' ','#'},
-                        {'#','#','#','#','#','#','#','#'}};
-
-        PlayerFactory playerFactory = new PlayerFactory(pacManSprites); //create player
-        Player player = playerFactory.createPacMan();
-
-        Direction east = Direction.valueOf("EAST"); // set players direction east
-        player.setDirection(east);
-
-        List<Player> playerList = new ArrayList(); // create list of players
-        playerList.add(player);
-
-        Level level = ghostMapParser.parseMap(map); // create level
-        level.registerPlayer(player);
-
-        game = new Game(pointCalculator) { // create game
-            @Override
-            public List<Player> getPlayers() {
-                return playerList;
-            }
-
-            @Override
-            public Level getLevel() {
-                return level;
-            }
-        };
-
-        launcher = new Launcher();
-        launcher.withMapFile("/test.txt");
-        launcher.launch();
+    void setUp() {
+        PacManSprites sprites = new PacManSprites();
+        GhostFactory ghostFactory = new GhostFactory(sprites);
+        LevelFactory levelFactory = new LevelFactory(
+            sprites,
+           ghostFactory,
+            mock(PointCalculator.class));
+        ghostMapParser = new GhostMapParser(levelFactory, new BoardFactory(sprites), ghostFactory);
+        PlayerFactory playerFactory = new PlayerFactory(sprites);
+        player = playerFactory.createPacMan();
     }
 
     /**
-     *
+     * Test the case where Pacman is 8 squares away from Clyde.
+     * Clyde should move towards Pacman.
      */
     @Test
     public void TestClydeFarFromPacman(){
-        launcher.getGame();
-        Game game = launcher.getGame();
-        Player player = game.getPlayers().get(0);
 
-        // start cleanly.
-        assertThat(game.isInProgress()).isFalse();
-        game.start();
-        assertThat(game.isInProgress()).isTrue();
-        assertThat(player.getScore()).isZero();
+        Level level = ghostMapParser.parseMap(Lists.newArrayList("############",
+            "#P.      .C#", "############"));
+        Board board = level.getBoard();
 
-        game.move(player, Direction.EAST); //Get points after eating a pellet
-        game.move(player, Direction.EAST);
-        game.move(player, Direction.EAST);
-        game.move(player, Direction.EAST);
-        game.move(player, Direction.EAST);
-        game.move(player, Direction.EAST);
-        assertEquals(player.getScore(),60);
+        level.registerPlayer(player);
+        player.setDirection(Direction.WEST);
+
+        Ghost clyde = Navigation.findUnitInBoard(Clyde.class, board);
+
+        Optional direction = clyde.nextAiMove();
+
+        assertEquals(direction, Optional.of(Direction.WEST));
+
+
     }
 
     /**
-     *
+     * Test the case where Pacman is 1 square away from Clyde.
+     * Clyde should move away from Pacman.
      */
     @Test
     public void TestClydeCloseFromPacman(){
-        assertThat(temp).isEqualTo(true);
+        Level level = ghostMapParser.parseMap(Lists.newArrayList("############",
+            "#P.C       #", "############"));
+        Board board = level.getBoard();
+
+        level.registerPlayer(player);
+        player.setDirection(Direction.WEST);
+
+        Ghost clyde = Navigation.findUnitInBoard(Clyde.class, board);
+
+        Optional direction = clyde.nextAiMove();
+
+        assertEquals(direction, Optional.of(Direction.EAST));
     }
 
     /**
-     *
+     * Test the case where Pacman isn't on the board.
+     * Clyde shouldn't move since his movement is based on Pacman.
      */
     @Test
     public void TestClydeWhenPacmanNull(){
-        assertThat(temp).isEqualTo(true);
+        Level level = ghostMapParser.parseMap(Lists.newArrayList("############",
+            "#C         #", "############"));
+        Board board = level.getBoard();
+
+        Ghost clyde = Navigation.findUnitInBoard(Clyde.class, board);
+        Optional direction = clyde.nextAiMove();
+
+        assertEquals(direction, Optional.empty());
     }
 
     /**
-     *
+     * Test the case where there is a null square between Pacman and Clyde.
+     * Clyde shouldn't move since there doesn't exist a shortest path between Clyde and Pacman.
      */
     @Test
-    public void TestClydeWhenGridSquarNull(){
-        assertThat(temp).isEqualTo(true);
+    public void TestClydeWhenGridSquareNull(){
+        Level level = ghostMapParser.parseMap(Lists.newArrayList("############",
+            "#P    #   C#", "############"));
+        Board board = level.getBoard();
+
+        level.registerPlayer(player);
+        player.setDirection(Direction.WEST);
+
+        Ghost clyde = Navigation.findUnitInBoard(Clyde.class, board);
+
+        Optional direction = clyde.nextAiMove();
+
+        assertEquals(direction, Optional.empty());
+
     }
 }
