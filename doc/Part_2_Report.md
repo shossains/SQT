7c9ee5358d6b155ced40fb51d5c0d3209fea61af
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2019-05-30 20:34:57 +0200

    Finished writing good and bad weather tests for exercises 1 and 2.

--- doc/Part_2_Report.md
+++ doc/Part_2_Report.md
@@ -2,10 +2,27 @@
 
 ### Exercise 1
 Some good weather cases we thought of:
+ - Verifying if methods from mocked classes in method makeGhostSquare are used.
+ - Test for empty space ( ) in addSquare method.
+ - Test for wall (#) in addSquare method.
+ - Test for pellet (.) in addSquare method.
+ - Test for ghost (G) in addSquare method.
+ - Test for pacman (P) in addSquare method.
 
-
+ 
 ### Exercise 2
-
+Some bad weather cases we thought of:
+ - Test if exception is thrown when an invalid char is used in addSquare method.
+ - Test if exception is thrown when an invalid position is used in addSquare method.
+ - Test if exception is thrown when an text map is null in parseMap method.
+ - Test if exception is thrown when an empty ArrayList is used in parseMap method.
+ - Test if exception is thrown when an only an empty string inside 
+ the ArrayList is used in parseMap method.
+ - Test if exception is thrown when text map lines, which are not of equal width,
+  are used in parseMap method.
+ - Test if exception is thrown when an empty string filename is used in parseMap method.
+ - Test if exception is thrown when an inputstream with an empty resource is used in parseMap method.
+ 
 ### Exercise 3
 
 ### Exercise 4
--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,5 +1,7 @@
 package nl.tudelft.jpacman.level;
 
+import com.google.common.collect.Lists;
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.Ghost;
@@ -7,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,31 +18,58 @@ import static org.mockito.Mockito.*;
 
 public class MapParserTest {
 
-    @Mock
+    /**
+     * Mocked LevelFactory, used for creating the MapParser and verifying if methods of this class are used.
+     */
     private LevelFactory levelFactory = mock(LevelFactory.class);
+
+    /**
+     * Mocked BoardFactory, used for creating the MapParser and verifying if methods of this class are used.
+     */
     private BoardFactory boardFactory = mock(BoardFactory.class);
 
+    /**
+     * Mocked ghost object.
+     */
     private Ghost ghost = mock(Ghost.class);
+
+    /**
+     * Mocked list with ghost objects.
+     */
     private List<Ghost> ghosts = mock(List.class);
+
+    /**
+     * Mocked list with Square objects.
+     */
     private List<Square> startingPositions = mock(List.class);
 
+    /**
+     * MapParser object.
+     */
     private MapParser parser;
+
+    /**
+     * 2 x 2 Grid consisting of mocked Square objects.
+     */
     private Square[][] grid = {
         { mock(Square.class), mock(Square.class) },
         { mock(Square.class), mock(Square.class) }
     };
 
+    /**
+     * Setup for each test for re-usability purposes.
+     */
     @BeforeEach
     void setup() {
         parser = new MapParser(levelFactory, boardFactory);
-
+        when(levelFactory.createGhost()).thenReturn(ghost);
     }
 
     /**
      * Tests if mocked classes are called in the method makeGhostSquare.
      */
     @Test
-    public void testMakeGhostSquare() {
+    void testMakeGhostSquare() {
         parser.makeGhostSquare(ghosts, ghost);
 
         verify(boardFactory).createGround();
@@ -50,10 +81,9 @@ public class MapParserTest {
      * Test for when empty space ( ) is used in addSquare method.
      */
     @Test
-    public void testAddSquareGround() {
+    void testAddSquareGround() {
         parser.addSquare(grid, ghosts, startingPositions, 1, 0, ' ');
         assertEquals(boardFactory.createGround(), grid[1][0]);
-        verify(boardFactory).createGround();
         verify(boardFactory, times(2)).createGround();
     }
 
@@ -61,10 +91,9 @@ public class MapParserTest {
      * Test for when wall (#) is used in addSquare method.
      */
     @Test
-    public void testAddSquareWall() {
+    void testAddSquareWall() {
         parser.addSquare(grid, ghosts, startingPositions, 0, 0, '#');
         assertEquals(boardFactory.createWall(), grid[0][0]);
-        verify(boardFactory).createWall();
         verify(boardFactory, times(2)).createWall();
     }
 
@@ -72,7 +101,7 @@ public class MapParserTest {
      * Test for when wall (.) is used in addSquare method.
      */
     @Test
-    public void testAddSquarePellet() {
+    void testAddSquarePellet() {
         Pellet pellet = mock(Pellet.class);
         when(levelFactory.createPellet()).thenReturn(pellet);
         parser.addSquare(grid, ghosts, startingPositions, 0, 1, '.');
@@ -84,22 +113,98 @@ public class MapParserTest {
      * Test for when wall (G) is used in addSquare method.
      */
     @Test
-    public  void testAddSquareGhost() {
-        when(levelFactory.createGhost()).thenReturn(mock(Ghost.class));
+    void testAddSquareGhost() {
         parser.addSquare(grid, ghosts, startingPositions, 1, 1, 'G');
         assertNull(grid[1][1]);
-        verify(levelFactory).createGhost();
+        verify(levelFactory, times(1)).createGhost();
     }
 
     /**
      * Test for when Pacman (P) is used in addSquare method.
      */
     @Test
-    public void testAddSquarePacMan() {
+    void testAddSquarePacMan() {
         parser.addSquare(grid, ghosts, startingPositions, 0, 0, 'P');
         assertEquals(boardFactory.createGround(), grid[0][0]);
-        verify(boardFactory).createGround();
         verify(boardFactory, times(2)).createGround();
+    }
+
+    /**
+     * Test if addSquare method throws an exception when an invalid character is used.
+     */
+    @Test
+    void testAddSquareInvalidChar() {
+        assertThrows(PacmanConfigurationException.class, () ->
+            parser.addSquare(grid, ghosts, startingPositions, 0, 0, 'X')
+        );
+    }
+
+    /**
+     * Test if addSquare method throws an exception when an incorrect coordinate is accessed.
+     */
+    @Test
+    void testAddSquareInvalidPosition() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+            parser.addSquare(grid, ghosts, startingPositions, 0, 3, 'G')
+        );
+    }
+
+    /**
+     * Test if parseMap throws an exception when the text map is null.
+     */
+    @Test
+    void testParseMapNull() {
+        ArrayList<String> text = null;
+        assertThrows(PacmanConfigurationException.class,
+            ()-> parser.parseMap(text));
+    }
+
+    /**
+     * Test if parseMap throws an exception when the text map is empty ArrayList.
+     */
+    @Test
+    void testParseMapEmptyInput() {
+        ArrayList<String> text = Lists.newArrayList();
+        assertThrows(PacmanConfigurationException.class,
+            ()-> parser.parseMap(text));
+    }
+
+    /**
+     * Test if parseMap throws an exception when the text map is an ArrayList with an empty string.
+     */
+    @Test
+    void testParseMapEmptyLine() {
+        ArrayList<String> text = Lists.newArrayList("");
+        assertThrows(PacmanConfigurationException.class,
+            ()-> parser.parseMap(text));
+    }
+
+    /**
+     * Test if parseMap throws an exception when the text map lines are not of equal width.
+     */
+    @Test
+    void testParseMapInvalidSize() {
+        ArrayList<String> text = Lists.newArrayList("##", "###");
+        assertThrows(PacmanConfigurationException.class,
+            ()-> parser.parseMap(text));
+    }
+
+    /**
+     * Test if parseMap throws an exception when the input file name is empty.
+     */
+    @Test
+    void testParseMapNoFile() {
+        String mapName = "";
+        assertThrows(PacmanConfigurationException.class, () -> parser.parseMap(mapName));
+    }
+
+    /**
+     * Test if parseMap throws an exception when the InputStream is empty.
+     */
+    @Test
+    void testParseMapNoInputSource() {
+        InputStream source = MapParser.class.getResourceAsStream("");
+        assertThrows(PacmanConfigurationException.class, () -> parser.parseMap(source));
     }
 
 
