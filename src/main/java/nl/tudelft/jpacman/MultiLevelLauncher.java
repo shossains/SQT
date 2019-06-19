f0cc68b5549f105a9e2c8559866ebc25b42f2e9b
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2019-06-19 15:48:22 +0200

    Worked on multilevel

--- src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -1,81 +1,39 @@
 package nl.tudelft.jpacman;
 
-import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.util.List;
-
-import nl.tudelft.jpacman.board.BoardFactory;
-import nl.tudelft.jpacman.board.Direction;
-import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.game.GameFactory;
+import nl.tudelft.jpacman.game.MultiLevelGame;
 import nl.tudelft.jpacman.level.Level;
-import nl.tudelft.jpacman.level.LevelFactory;
-import nl.tudelft.jpacman.level.MapParser;
-import nl.tudelft.jpacman.level.Player;
-import nl.tudelft.jpacman.level.PlayerFactory;
-import nl.tudelft.jpacman.npc.ghost.GhostFactory;
 import nl.tudelft.jpacman.points.PointCalculator;
 import nl.tudelft.jpacman.points.PointCalculatorLoader;
-import nl.tudelft.jpacman.sprite.PacManSprites;
-import nl.tudelft.jpacman.ui.Action;
 import nl.tudelft.jpacman.ui.PacManUI;
 import nl.tudelft.jpacman.ui.PacManUiBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Creates and launches the JPacMan UI.
  *
- * @author Jeroen Roosen
  */
-@SuppressWarnings("PMD.TooManyMethods")
-public class MultiLevelLauncher {
+public class MultiLevelLauncher extends Launcher {
 
-    private static final PacManSprites SPRITE_STORE = new PacManSprites();
+    private final int amountlvl = 4;
+    private MultiLevelGame multiGame;
+    private static String[] mapStrings = {"/board.txt", "/boardTest.txt", "/boardTest.txt", "/board.txt"};
 
-    public static final String DEFAULT_MAP = "/board.txt";
-    private String levelMap = DEFAULT_MAP;
 
-    private PacManUI pacManUI;
-    private Game game;
-
-    /**
-     * @return The game object this launcher will start when {@link #launch()}
-     *         is called.
-     */
-    public Game getGame() {
-        return game;
+    @Override
+    public MultiLevelGame getGame() {
+        return multiGame;
     }
 
-    /**
-     * The map file used to populate the level.
-     *
-     * @return The name of the map file.
-     */
-    protected String getLevelMap() {
-        return levelMap;
-    }
-
-    /**
-     * Set the name of the file containing this level's map.
-     *
-     * @param fileName
-     *            Map to be used.
-     * @return Level corresponding to the given map.
-     */
-    public Launcher withMapFile(String fileName) {
-        levelMap = fileName;
-        return this;
-    }
-
-    /**
-     * Creates a new game using the level from {@link #makeLevel()}.
-     *
-     * @return a new Game.
-     */
-    public Game makeGame() {
+    @Override
+    public MultiLevelGame makeGame() {
         GameFactory gf = getGameFactory();
-        Level level = makeLevel();
-        game = gf.createSinglePlayerGame(level, loadPointCalculator());
-        return game;
+        List<Level> levels = makeLevels();
+        multiGame = gf.createMultiLevelGame(levels, loadPointCalculator());
+        return multiGame;
     }
 
     private PointCalculator loadPointCalculator() {
@@ -83,131 +41,27 @@ public class MultiLevelLauncher {
     }
 
     /**
-     * Creates a new level. By default this method will use the map parser to
-     * parse the default board stored in the <code>board.txt</code> resource.
+     * Creates List containing multiple levels.
      *
-     * @return A new level.
+     * @return List of levels.
      */
-    public Level makeLevel() {
+    public List<Level> makeLevels() {
         try {
-            return getMapParser().parseMap(getLevelMap());
+            List<Level> levels = new ArrayList<>();
+            for (int i = 0; i < amountlvl; i++) {
+                Level level = getMapParser().parseMap(mapStrings[i]);
+                levels.add(level);
+            }
+            return levels;
         } catch (IOException e) {
             throw new PacmanConfigurationException(
-                "Unable to create level, name = " + getLevelMap(), e);
+                "Unable to create levels, name = " + getLevelMap(), e);
         }
     }
 
-    /**
-     * @return A new map parser object using the factories from
-     *         {@link #getLevelFactory()} and {@link #getBoardFactory()}.
-     */
-    protected MapParser getMapParser() {
-        return new MapParser(getLevelFactory(), getBoardFactory());
+    public static void main(String[] args) {
+        new MultiLevelLauncher().launch();
     }
 
-    /**
-     * @return A new board factory using the sprite store from
-     *         {@link #getSpriteStore()}.
-     */
-    protected BoardFactory getBoardFactory() {
-        return new BoardFactory(getSpriteStore());
-    }
 
-    /**
-     * @return The default {@link PacManSprites}.
-     */
-    protected PacManSprites getSpriteStore() {
-        return SPRITE_STORE;
-    }
-
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}
-     *         and the ghosts from {@link #getGhostFactory()}.
-     */
-    protected LevelFactory getLevelFactory() {
-        return new LevelFactory(getSpriteStore(), getGhostFactory(), loadPointCalculator());
-    }
-
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}.
-     */
-    protected GhostFactory getGhostFactory() {
-        return new GhostFactory(getSpriteStore());
-    }
-
-    /**
-     * @return A new factory using the players from {@link #getPlayerFactory()}.
-     */
-    protected GameFactory getGameFactory() {
-        return new GameFactory(getPlayerFactory());
-    }
-
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}.
-     */
-    protected PlayerFactory getPlayerFactory() {
-        return new PlayerFactory(getSpriteStore());
-    }
-
-    /**
-     * Adds key events UP, DOWN, LEFT and RIGHT to a game.
-     *
-     * @param builder
-     *            The {@link PacManUiBuilder} that will provide the UI.
-     */
-    protected void addSinglePlayerKeys(final PacManUiBuilder builder) {
-        builder.addKey(KeyEvent.VK_UP, moveTowardsDirection(Direction.NORTH))
-            .addKey(KeyEvent.VK_DOWN, moveTowardsDirection(Direction.SOUTH))
-            .addKey(KeyEvent.VK_LEFT, moveTowardsDirection(Direction.WEST))
-            .addKey(KeyEvent.VK_RIGHT, moveTowardsDirection(Direction.EAST));
-    }
-
-    private Action moveTowardsDirection(Direction direction) {
-        return () -> {
-            assert game != null;
-            getGame().move(getSinglePlayer(getGame()), direction);
-        };
-    }
-
-    private Player getSinglePlayer(final Game game) {
-        List<Player> players = game.getPlayers();
-        if (players.isEmpty()) {
-            throw new IllegalArgumentException("Game has 0 players.");
-        }
-        return players.get(0);
-    }
-
-    /**
-     * Creates and starts a JPac-Man game.
-     */
-    public void launch() {
-        makeGame();
-        PacManUiBuilder builder = new PacManUiBuilder().withDefaultButtons();
-        addSinglePlayerKeys(builder);
-        pacManUI = builder.build(getGame());
-        pacManUI.start();
-    }
-
-    /**
-     * Disposes of the UI. For more information see
-     * {@link javax.swing.JFrame#dispose()}.
-     *
-     * Precondition: The game was launched first.
-     */
-    public void dispose() {
-        assert pacManUI != null;
-        pacManUI.dispose();
-    }
-
-    /**
-     * Main execution method for the Launcher.
-     *
-     * @param args
-     *            The command line arguments - which are ignored.
-     * @throws IOException
-     *             When a resource could not be read.
-     */
-    public static void main(String[] args) throws IOException {
-        new Launcher().launch();
-    }
 }
--- src/main/java/nl/tudelft/jpacman/game/MultiLevelGame.java
+++ src/main/java/nl/tudelft/jpacman/game/MultiLevelGame.java
@@ -1,5 +1,6 @@
 package nl.tudelft.jpacman.game;
 
+import com.google.common.collect.ImmutableList;
 import nl.tudelft.jpacman.level.Level;
 import nl.tudelft.jpacman.level.Player;
 import nl.tudelft.jpacman.points.PointCalculator;
@@ -7,7 +8,7 @@ import nl.tudelft.jpacman.points.PointCalculator;
 import java.util.List;
 
 /**
- *
+ * A game with one player and multiple levels.
  */
 public class MultiLevelGame extends Game {
 
@@ -17,25 +18,40 @@ public class MultiLevelGame extends Game {
     private Player player;
 
     /**
-     * The level of this game.
+     * List of levels of this game.
      */
-    private Level level;
+    private List<Level> levels;
 
     /**
      *
+     * @param player  The player.
+     * @param levels List of levels.
      * @param pointCalculator The way to calculate points upon collisions.
      */
-    protected MultiLevelGame(PointCalculator pointCalculator) {
+    protected MultiLevelGame(Player player, List<Level> levels, PointCalculator pointCalculator) {
         super(pointCalculator);
+        this.player = player;
+        this.levels = levels;
     }
 
     @Override
     public Level getLevel() {
-        return level;
+        return levels.get(0);
     }
 
     @Override
     public List<Player> getPlayers() {
-        return null;
+        return ImmutableList.of(player);
+    }
+
+    @Override
+    public void levelWon() {
+        if (levels.isEmpty()) {
+            System.out.println("MultiLevelGame.java line 50");
+            stop();
+        } else {
+            System.out.println("MultilevelGame.java line 53 \n");
+            getLevel().start();
+        }
     }
 }
