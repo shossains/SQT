19df782eef6a1a9675352478512cdc8494ad2e74
Shaan <s.hossain@student.tudelft.nl>
2019-06-21 17:35:34 +0200

    gradle fix

--- src/test/java/nl/tudelft/jpacman/game/GameStateMultiLauncherTest.java
+++ src/test/java/nl/tudelft/jpacman/game/GameStateMultiLauncherTest.java
@@ -7,7 +7,11 @@ import nl.tudelft.jpacman.board.Direction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * Test class for Game using MultiLevelLauncher.
